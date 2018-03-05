dd0185b6ea8fa7fdcc52c377df8d6673df7fe4db
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-03-05 10:28:32 +0000

    test: Improve ZIP tests

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -9,8 +9,9 @@ class TestZIPDetector(TestCase):
         self.assertEqual(zip.check('tests/samples/zip/regular.zip'), PolyglotLevel.VALID)
 
     def test_check_garbage_at_the_beginning(self):
-        self.assertTrue(zip.check('tests/samples/zip/garbage_at_beginning.zip') & PolyglotLevel.GARBAGE_AT_BEGINNING)
+        self.assertEqual(zip.check('tests/samples/zip/garbage_at_beginning.zip'),
+                         PolyglotLevel.VALID | PolyglotLevel.GARBAGE_AT_BEGINNING)
 
     def test_check_garbage_at_the_end(self):
-        self.assertTrue(zip.check('tests/samples/zip/garbage_at_end.zip') & PolyglotLevel.GARBAGE_AT_END,
-                        "Should detect GARBAGE_AT_END")
+        self.assertEqual(zip.check('tests/samples/zip/garbage_at_end.zip'),
+                         PolyglotLevel.VALID | PolyglotLevel.GARBAGE_AT_END)

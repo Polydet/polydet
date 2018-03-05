d3a675088796b1f0262ab086dd40bef7d1cac870
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-03-05 10:38:34 +0000

    fix: Fix ValueError with mmap on empty file

--- polyglot_detector/plugins/pdf.py
+++ polyglot_detector/plugins/pdf.py
@@ -4,16 +4,21 @@ from polyglot_detector.polyglot_level import PolyglotLevel
 
 FILE_EXTENSION = 'PDF'
 
+
 # TODO Check if the magic is within the first 1024 bytes to declare VALID ?
+# TODO Add garbage_at_end
 def check(filename):
     magic = b"%PDF-"
 
-    with open(filename, 'rb') as file, \
-            mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-        magic_index = s.find(magic)
-        if magic_index == -1:
+    with open(filename, 'rb') as file:
+        try:
+            with mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+                magic_index = s.find(magic)
+                if magic_index == -1:
+                    return None
+                elif magic_index == 0:
+                    return PolyglotLevel.VALID
+                else:
+                    return PolyglotLevel.VALID | PolyglotLevel.GARBAGE_AT_BEGINNING
+        except ValueError:  # mmap raise ValueError if empty file
             return None
-        elif magic_index == 0:
-            return PolyglotLevel.VALID
-        else:
-            return PolyglotLevel.VALID | PolyglotLevel.GARBAGE_AT_BEGINNING
--- polyglot_detector/plugins/zip.py
+++ polyglot_detector/plugins/zip.py
@@ -9,17 +9,20 @@ EOCD_MIN_SIZE = 22
 
 # TODO Return GARBAGE_AT_END only if garbage after comment ?
 def check(filename):
-    with open(filename, 'rb') as file, \
-            mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-        size = s.size()
-        magic_index = s.rfind(MAGIC, 0, max(0, size - EOCD_MIN_SIZE + len(MAGIC)))
-        if magic_index == -1:
+    with open(filename, 'rb') as file:
+        try:
+            with mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+                size = s.size()
+                magic_index = s.rfind(MAGIC, 0, max(0, size - EOCD_MIN_SIZE + len(MAGIC)))
+                if magic_index == -1:
+                    return None
+                else:
+                    size_of_eocd = size - magic_index
+                    flag = PolyglotLevel.VALID
+                    if s.find(MAGIC) != 0:
+                        flag |= PolyglotLevel.GARBAGE_AT_BEGINNING
+                    if size_of_eocd != EOCD_MIN_SIZE:
+                        flag |= PolyglotLevel.GARBAGE_AT_END
+                    return flag
+        except ValueError:  # mmap raise ValueError if empty file
             return None
-        else:
-            size_of_eocd = size - magic_index
-            flag = PolyglotLevel.VALID
-            if s.find(MAGIC) != 0:
-                flag |= PolyglotLevel.GARBAGE_AT_BEGINNING
-            if size_of_eocd != EOCD_MIN_SIZE:
-                flag |= PolyglotLevel.GARBAGE_AT_END
-            return flag

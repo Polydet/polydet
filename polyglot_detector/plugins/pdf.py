13a6033c2c7b28839a946dafd97ccbfaa58f6b56
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-04-23 15:22:36 +0200

    feat: add INVALID tests for PDF

--- polyglot_detector/plugins/pdf.py
+++ polyglot_detector/plugins/pdf.py
@@ -1,33 +1,50 @@
 import mmap
+import re
 from polyglot_detector.polyglot_level import PolyglotLevel
 
 
 FILE_EXTENSION = 'pdf'
 
 
-_PDF_EOF = b'\n%%EOF'
+__PDF_MAGIC = b'%PDF-'
+__PDF_EOF = b'\n%%EOF'
+
+__PDF_FULL_MAGIC_LEN = 9
+__PDF_FULL_MAGIC_RE = re.compile(b'%PDF-[12].\d\n')
 
 
-# TODO Check if the magic is within the first 1024 bytes to declare VALID ?
 def check(filename):
-    magic = b"%PDF-"
 
     with open(filename, 'rb') as file:
         try:
-            with mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-                magic_index = s.find(magic)
-                if magic_index == -1:
+            with mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as buf:
+
+                magic_offset = buf.find(__PDF_MAGIC)
+                if magic_offset == -1:
                     return None
-                flag = PolyglotLevel.VALID
-                if magic_index != 0:
+
+                if magic_offset <= 1024 and is_magic_valid(buf, magic_offset):
+                    flag = PolyglotLevel.VALID
+                else:
+                    flag = PolyglotLevel.INVALID
+
+                if magic_offset != 0:
                     flag |= PolyglotLevel.GARBAGE_AT_BEGINNING
-                if has_garbage_at_end(s):
+                if has_garbage_at_end(buf):
                     flag |= PolyglotLevel.GARBAGE_AT_END
+
                 return flag
+
         except ValueError:  # mmap raise ValueError if empty file
             return None
 
 
-def has_garbage_at_end(buffer) -> bool:
-    eof_index = buffer.find(_PDF_EOF)
-    return eof_index != -1 and eof_index + len(_PDF_EOF) + 1 < buffer.size()  # +1 for potential \n
+def is_magic_valid(buf, magic_offset) -> bool:
+    buf.seek(magic_offset)
+    full_magic = buf.read(__PDF_FULL_MAGIC_LEN)
+    return __PDF_FULL_MAGIC_RE.match(full_magic) is not None
+
+
+def has_garbage_at_end(buf) -> bool:
+    eof_index = buf.find(__PDF_EOF)
+    return eof_index != -1 and eof_index + len(__PDF_EOF) + 1 < buf.size()  # +1 for potential \n
--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -9,9 +9,13 @@ class TestPDFDetector(TestCase):
         result = pdf.check('tests/samples/pdf/regular.pdf')
         self.assertEqual(PolyglotLevel.VALID, result)
 
+    def test_not_pdf(self):
+        result = pdf.check('tests/samples/zip/regular.zip')
+        self.assertIsNone(result)
+
     def test_truncated_magic(self):
         result = pdf.check('tests/samples/pdf/truncated_magic.pdf')
-        self.assertEqual(PolyglotLevel.VALID, result)
+        self.assertEqual(PolyglotLevel.INVALID, result)
 
     def test_magic_within_1024_first_bytes(self):
         result = pdf.check('tests/samples/pdf/garbage_at_beginning.pdf')
@@ -19,7 +23,7 @@ class TestPDFDetector(TestCase):
 
     def test_magic_anywhere_in_the_file(self):
         result = pdf.check('tests/samples/pdf/lot_of_garbage_at_beginning.pdf')
-        self.assertEqual(PolyglotLevel.VALID | PolyglotLevel.GARBAGE_AT_BEGINNING, result)
+        self.assertEqual(PolyglotLevel.INVALID | PolyglotLevel.GARBAGE_AT_BEGINNING, result)
 
     def test_garbage_at_end(self):
         result = pdf.check('tests/samples/pdf/garbage_at_end.pdf')

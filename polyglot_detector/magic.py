2c7b1111b4f0c8c430dac496510f41f5e2590df1
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-06-07 18:18:50 +0200

    feat(magic): add extensions for exe and elf

--- polyglot_detector/magic.py
+++ polyglot_detector/magic.py
@@ -1,30 +1,39 @@
 import magic
 import mimetypes
 
-__MAGIC = None
-""":type __MAGIC: magic.Magic"""
+__magic = None
+""":type __magic: magic.Magic"""
 
 __OCTET_STREAM_MIME = 'application/octet-stream'
 
+__inited = False
 
-def __get_instance() -> magic.Magic:
-    global __MAGIC
-    if __MAGIC is None:
-        __MAGIC = magic.open(magic.MAGIC_MIME_TYPE | magic.MAGIC_CONTINUE | magic.MAGIC_RAW)
-        __MAGIC.load()
-    return __MAGIC
+
+def __init():
+    global __inited, __magic
+    if __inited:
+        return
+    __inited = True
+    __magic = magic.open(magic.MAGIC_MIME_TYPE | magic.MAGIC_CONTINUE | magic.MAGIC_RAW)
+    __magic.load()
+    mimetypes.add_type('application/x-sharedlib', '.elf', strict=True)
+    mimetypes.add_type('application/x-executable', '.elf', strict=True)
+    mimetypes.add_type('application/x-dosexec', '.exe', strict=True)
 
 
 def set_magic_file(path):
-    __get_instance().load(path)
+    __init()
+    __magic.load(path)
 
 
 def magic_scan(filename):
     """Scan for a single file type with libmagic"""
-    global __MAGIC
+    global __magic
+
+    __init()
 
     results = []
-    file_mimes = __get_instance().file(filename)
+    file_mimes = __magic.file(filename)
     for file_mime in file_mimes.split('\012- '):
         if file_mime != __OCTET_STREAM_MIME:
             extension = mimetypes.guess_extension(file_mime)

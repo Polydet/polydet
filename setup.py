b45c632da6da4067aa34cb771f12674843bdf4b7
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-06-23 23:14:11 +0200

    chore: change version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='polydet',
-      version='0.1',
+      version='0.2',
       description='Detect suspicious data in files',
       author='Julien Campion, Hugo Laloge',
       author_email='jlc72@kent.ac.uk, hljl2@kent.ac.uk',

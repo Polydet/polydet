922924c68eb8b86bab380ba0f06679dee27f31ad
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-07-05 17:17:11 +0200

    test(swf): add tests for FWS file

--- tests/test_swf.py
+++ tests/test_swf.py
@@ -6,6 +6,7 @@ from polydet.plugins import swf
 
 class TestSWFDetector(TestCase):
     def test_regular(self):
+        self.assertEqual(PolyglotLevel(), swf.check('tests/samples/swf/regular.fws9'))
         self.assertEqual(PolyglotLevel(), swf.check('tests/samples/swf/regular.cws9'))
         self.assertEqual(PolyglotLevel(), swf.check('tests/samples/swf/regular.zws13'))
 
@@ -13,6 +14,8 @@ class TestSWFDetector(TestCase):
         self.assertIsNone(swf.check('tests/samples/zip/regular.zip'))
 
     def test_garbage_at_end(self):
+        self.assertEqual(PolyglotLevel(suspicious_chunks=[(0x1EC, 0xB5)]),
+                         swf.check('tests/samples/swf/garbage-at-end.fws9'))
         self.assertEqual(PolyglotLevel(suspicious_chunks=[(0x16B, 0xB5)]),
                          swf.check('tests/samples/swf/garbage-at-end.cws9'))
         self.assertEqual(PolyglotLevel(suspicious_chunks=[(0x2A9, 0xB5)]),
